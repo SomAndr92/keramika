bc0b61a1ef7d854e46e28263da8c6edc368bf215
SomAndr92 <nikmos92@mail.ru>
2024-02-03 21:27:39 +0300

    настройка шапки

--- main.php
+++ main.php
@@ -23,17 +23,17 @@ if ($_POST['exit']) {
 
 </head>
 <body>
-<nav class="navbar bg-dark" data-bs-theme="dark" >
+<nav class="navbar bg-dark navbar-expand-lg" data-bs-theme="dark" >
   <div class="container">
     <a class="navbar-brand">КЕРАМИКА</a>
    <? if(!empty($_SESSION['login'])){?>
-    <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
           aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Переключатель навигации">
           <span class="navbar-toggler-icon"></span>
         </button> 
       
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div class="navbar-nav ">
+          <div class="navbar-nav d-flex align-items-center ">
             <a class="nav-link text-light me-4" href="#">О нас</a>
             <li class="nav-item  dropdown text-light me-4">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -48,22 +48,23 @@ if ($_POST['exit']) {
             <a class="nav-link text-light me-4" href="#">Клубная карта</a>
             <a class="nav-link text-light me-4" href="#">Отзывы</a>
             <a class="nav-link text-light me-4" href="#">Контакты</a>
+            <a class="nav-link text-light me-4" href="#"> <form method="post" class="d-flex align-items-center" >
+    
+    <div class="me-2 text-light">
+    <i class="fa-solid fa-user fa-lg" style="color: green;"></i>
+    
+      <?  echo $_SESSION['login'];
+      
+    ?>
+    
+  </div>
+    
+    <input class="btn btn-outline-warning" type="submit" name="exit" value="Выход">
+  </form></a>
           </div>
         </div>    
-   -->
-    <form method="post" class="d-flex align-items-center" >
-    
-      <div class="me-2 text-light">
-      <i class="fa-solid fa-user fa-lg" style="color: green;"></i>
-      
-        <?  echo $_SESSION['login'];
-        
-      ?>
-      
-    </div>
-      
-      <input class="btn btn-outline-warning" type="submit" name="exit" value="Выход">
-    </form><?
+   
+    <?
 }?>
   </div>
 </nav>  
